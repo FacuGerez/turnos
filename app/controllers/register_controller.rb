78e942bebfb62f6e928ceecaa95c9884f00116ae
FacuGerez <facundogerezuba@hotmail.com>
2023-05-26 14:10:20 -0300

    al dia

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -4,16 +4,24 @@ class RegisterController < ApplicationController
   end
 
   def create
+    @todosLosUsuarios = User.all
+    
     @user = User.new()
     @user[:name] =params[:name]
     @user[:surname] = params[:surname]
     @user[:email] = params[:email]
     @user[:password] = params[:password]
 
+    @todosLosUsuarios.each do |usu|
+      if usu[:email] == params[:email]
+        render :new, status: :unprocessable_entity, notice: "Este correo ya fue usado anteriormente"        
+      end
+    end
+
     if @user.save
       #hago algo
     else
-      render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity, notice: "Hay campos sin completar"
     end
     #if @user.save        osea que si se guarda
     #hago algo     (redirec_to user_path     osea la pagina del usuario que todavia no generamos)
