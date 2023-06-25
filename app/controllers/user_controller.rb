a4edb734325286b0cf411f589ade1a4134c4d840
FacuGerez <facundogerezuba@hotmail.com>
2023-06-25 10:09:21 -0300

    Arreglo errores usuario

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,8 @@
 class UserController < ApplicationController
   def index
     @user = User.find(params[:id])
-    @eventos = Evento.where(user_id: params[:id]).where(dia: Time.now.beginning_of_month.beginning_of_week..Time.now.end_of_month.end_of_week)
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @eventos = Evento.where(user_id: params[:id]).where(dia: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
     flash.clear
   end
 end
