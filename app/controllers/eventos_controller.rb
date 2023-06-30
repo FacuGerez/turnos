3984e32878788b01073e9ee17e13e910765c2a9b
FacuGerez <facundogerezuba@hotmail.com>
2023-06-30 10:06:36 -0300

    Ajustes edicion y creacion evento

--- app/controllers/eventos_controller.rb
+++ app/controllers/eventos_controller.rb
@@ -7,8 +7,9 @@ class EventosController < ApplicationController
 
   def new
   end
-  
+
   def create
+    parseo()
     @usuario = User.find(params[:id])
     @evenNuevo =  @usuario.eventos.build(evento_params)
 
@@ -23,13 +24,14 @@ class EventosController < ApplicationController
       flash[:success] = "El plazo o los horarios de empieza y termina estan mal puestos o no estan"
       render :new, status: :unprocessable_entity
     end
-    
+
   end
 
   def edit
   end
 
   def update
+    parseo()
     if validacion()
       if @evento.update(evento_params)
         redirect_to user_path(params[:id])
@@ -48,26 +50,29 @@ class EventosController < ApplicationController
     redirect_to user_path(params[:id]), status: :see_other
   end
 
-  def evento_params
-    params.require(:evento).permit(:eventName,:description,:start_time,:end_time,:plazoDeTiempo,:dia)
-  end
-
-  def validacion 
-    if params[:evento][:start_time] == "" || params[:evento][:end_time] == "" || params[:evento][:plazoDeTiempo] == ""
-      return false
-    end
-    empieza =  Time.parse(params[:evento][:start_time])
-    termina = Time.parse(params[:evento][:end_time])
-    plazoSeleccionado = Time.parse(params[:evento][:plazoDeTiempo])
-    diferenciaSegundos =  termina - empieza
-    diffMinutos = diferenciaSegundos / 60
-    diffPlazo = (plazoSeleccionado.hour * 60) + (plazoSeleccionado.min)
-    return diffMinutos > 0 && diffMinutos >= diffPlazo
-  end
-
   private
     def set_evento
       @evento = Evento.find(params[:id1])
+    end
+
+    def parseo
+      params[:evento].parse_time_select! :start_time
+      params[:evento].parse_time_select! :end_time
+      params[:evento].parse_time_select! :plazoDeTiempo
+    end
+
+    def evento_params
+      params.require(:evento).permit(:eventName,:description,:start_time,:end_time,:plazoDeTiempo,:dia)
+    end
+
+    def validacion
+      empieza =  params[:evento][:start_time]
+      termina = params[:evento][:end_time]
+      plazoSeleccionado = params[:evento][:plazoDeTiempo]
+      diferenciaSegundos =  termina - empieza
+      diffMinutos = diferenciaSegundos / 60
+      diffPlazo = (plazoSeleccionado.hour * 60) + (plazoSeleccionado.min)
+      return diffMinutos > 0 && diffMinutos >= diffPlazo
     end
 
 end
