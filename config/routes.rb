45189abae8eaaf9be30c9b097fe232b10e256c6f
FacuGerez <facundogerezuba@hotmail.com>
2023-07-02 18:33:48 -0300

    ruta de inicio

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "index#index"
   get '/register', to: "user#new", as: :new_user
   post "/register", to: "user#create", as: :create_user
   get '/login' , to: "user#login", as: :serch_user
