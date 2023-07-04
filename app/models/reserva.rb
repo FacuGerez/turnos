5cb78e12b2145bb3fbd6de243c3d07dabccbd1db
FacuGerez <facundogerezuba@hotmail.com>
2023-07-04 10:20:51 -0300

    Update errores reservas

--- app/models/reserva.rb
+++ app/models/reserva.rb
@@ -1,5 +1,14 @@
 class Reserva < ApplicationRecord
-    validates :email, presence: true 
+    validates :email, presence: true,
+        format: {
+            with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+            message: :invalid
+        }
     validates :horario, presence: true
     belongs_to :evento
+    before_save :downcase_attributes
+    private
+        def downcase_attributes
+            self.email = email.downcase
+        end
 end
